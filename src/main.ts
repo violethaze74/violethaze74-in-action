2ead0779ee267d9d558ff6a304345875c4cc226f
violethaze74 <65783327+violethaze74@users.noreply.github.com>
2022-01-30 16:00:11 -0500

    Initialize dependabot/npm_and_yarn/browserslist-4.16.6

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+// -*- mode: javascript; js-indent-level: 2 -*-
+
+import * as core from '@actions/core'
+import {SnapcraftBuilder} from './build'
+
+async function run(): Promise<void> {
+  try {
+    const projectRoot = core.getInput('path')
+    const includeBuildInfo =
+      (core.getInput('build-info') || 'true').toUpperCase() === 'TRUE'
+    core.info(`Building Snapcraft project in "${projectRoot}"...`)
+    const snapcraftChannel = core.getInput('snapcraft-channel')
+    const snapcraftArgs = core.getInput('snapcraft-args')
+    const uaToken = core.getInput('ua-token')
+
+    const builder = new SnapcraftBuilder({
+      projectRoot,
+      includeBuildInfo,
+      snapcraftChannel,
+      snapcraftArgs,
+      uaToken
+    })
+    await builder.build()
+    const snap = await builder.outputSnap()
+    core.setOutput('snap', snap)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
